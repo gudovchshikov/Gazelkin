b54ea74c5ba8f83d93964ec10a3d3cc2c689517a
gudovchshikov <gudovchshikov@icloud.com>
2023-02-26 17:54:01 +0500

    Upload project

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const headerLang = document.querySelector('.language__selected');
+
+headerLang.addEventListener('click', (event) => {
+
+
+    let headerOpen = document.querySelector('.language__change');
+    let headerWrapper = document.querySelector('.language__wrapper');
+
+
+    headerOpen.classList.toggle('active');
+    headerWrapper.classList.toggle('active');
+
+})
+  
+
+// -------------- Slider -----------------
+
+
+$('.reviews__items').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });

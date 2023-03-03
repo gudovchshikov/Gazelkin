a98fc100fa9cc3674f10d3e766afe4848dfa28ea
gudovchshikov <gudovchshikov@icloud.com>
2023-03-03 11:24:24 +0500

    complete adaptive

--- js/main.js
+++ js/main.js
@@ -33,8 +33,14 @@ $('.reviews__items').slick({
           infinite: true
         }
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
     ]
 });
+
